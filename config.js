b1be57402be76ceb4d61debacfbc33c9d1f4b51d
John Reichert <john@lobster.home>
2020-03-25 20:47:41 -0400

    start

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {};
+
+// The OAuth client ID from the Google Developers console.
+config.oAuthClientID = 'ADD YOUR CLIENT ID';
+
+// The OAuth client secret from the Google Developers console.
+config.oAuthclientSecret = 'ADD YOUR CLIENT SECRET';
+
+config.apiHost = 'https://photoslibrary.googleapis.com';
+
+config.authEndpoint = '/auth/photoslibrary.readonly';
+
+module.exports = config;
